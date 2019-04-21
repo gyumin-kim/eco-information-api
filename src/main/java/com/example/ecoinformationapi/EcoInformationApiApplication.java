ec138ab6b086d59021e8b2102f88d0790996b74c
Gyumin Kim <starkying@gmail.com>
2019-04-21 16:42:32 +0900

    Initial Commit

---
+++ src/main/java/com/example/ecoinformationapi/EcoInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecoinformationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoInformationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoInformationApiApplication.class, args);
+	}
+
+}
